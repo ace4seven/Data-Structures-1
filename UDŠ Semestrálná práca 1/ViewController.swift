e780807749e7ff47fb556bcddaa0a6174b1a5cdf
Juraj Macák <juraj.macak@goodrequest.com>
2018-10-02 09:04:24 +0200

    Initial Commit

---
+++ UDŠ Semestrálná práca 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UDŠ Semestrálná práca 1
+//
+//  Created by Juraj Macák on 02/10/2018.
+//  Copyright © 2018 Juraj Macák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
